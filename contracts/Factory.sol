afaa727c8eaa37aad3a2f33ac44b2f39a3a22568
MikeySharma <mandalpriyanshu22@gmail.com>
2025-01-10 15:37:29 +0545

    Create Factory

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.27;
+pragma solidity >=0.8.2 <0.9.0;
 
-contract Factory {}
+contract Factory {
+    uint256 public immutable fee;
+    address public owner;
+
+    constructor(uint256 _fee) {
+        fee = _fee;
+        owner = msg.sender;
+    }
+}
--- test/Factory.js
+++ test/Factory.js
@@ -3,5 +3,28 @@ const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
 describe("Factory", function () {
+    const FEE = ethers.parseUnits("0.01", 18);
 
-})
+    async function deployFactoryFixtures() {
+        // Fetch Accounts
+        const [deployer] = await ethers.getSigners();
+
+        // Fetch the contract
+        const Factory = await ethers.getContractFactory("Factory", deployer);
+        // Deploy the contract
+        const factory = await Factory.deploy(FEE);
+
+        return { factory, deployer };
+    }
+
+    describe("Deployment", function () {
+        it("Should set the fee", async function () {
+            const { factory } = await loadFixture(deployFactoryFixtures);
+            expect(await factory.fee()).to.equal(FEE);
+        });
+        it("Should set the owner", async function () {
+            const { factory, deployer } = await loadFixture(deployFactoryFixtures);
+             expect(await factory.owner()).to.equal(deployer.address);
+        });
+    });
+});
