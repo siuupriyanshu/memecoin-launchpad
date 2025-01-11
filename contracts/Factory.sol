b3ef2aee7f1d81412ed57582cdb8a6c7cc4200d2
MikeySharma <mandalpriyanshu22@gmail.com>
2025-01-11 14:13:41 +0545

    List Token

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,12 +1,65 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.2 <0.9.0;
+import {Token} from "./Token.sol";
 
 contract Factory {
     uint256 public immutable fee;
     address public owner;
 
+    uint256 public totalTokens;
+    address[] public tokens;
+    mapping(address => TokenSale) public tokenToSale;
+
+    struct TokenSale {
+        address token;
+        string name;
+        address creator;
+        uint256 sold;
+        uint256 raised;
+        bool isOpen;
+    }
+
+    event Created(address indexed token);
+
     constructor(uint256 _fee) {
         fee = _fee;
         owner = msg.sender;
+    }
+
+    function getTokenSale(uint256 _index) public view returns (TokenSale memory) {
+        return tokenToSale[tokens[_index]];
+    }
+
+    function create(
+        string memory _name,
+        string memory _symbol
+    ) external payable {
+        // Make sure that the fee is correct
+
+        require(msg.value == fee, "Factory: creator fee not met");
+
+        // Create a new Token
+        Token token = new Token(msg.sender, _name, _symbol, 1_000_000 ether);
+
+        // Save the token for later use
+        tokens.push(address(token));
+
+        totalTokens++;
+
+        // List the token for sale
+        TokenSale memory sale = TokenSale(
+            address(token),
+            _name,
+            msg.sender,
+            0,
+            0,
+            true
+        );
+
+        tokenToSale[address(token)] = sale;
+
+        // Tell people it's live
+
+        emit Created(address(token));
     }
 }
--- test/Factory.js
+++ test/Factory.js
@@ -7,14 +7,24 @@ describe("Factory", function () {
 
     async function deployFactoryFixtures() {
         // Fetch Accounts
-        const [deployer] = await ethers.getSigners();
+        const [deployer, creator, buyer] = await ethers.getSigners();
 
         // Fetch the contract
         const Factory = await ethers.getContractFactory("Factory", deployer);
+
         // Deploy the contract
         const factory = await Factory.deploy(FEE);
 
-        return { factory, deployer };
+        // Create token
+        const transcation = await factory.connect(creator).create("Dapp Uni", "DAPP", { value: FEE })
+        await transcation.wait();
+
+        // Get token address
+        const tokenAddress = await factory.tokens(0);
+        const token = await ethers.getContractAt("Token", tokenAddress)
+
+        // Return values
+        return { factory, deployer, creator, token, buyer };
     }
 
     describe("Deployment", function () {
@@ -24,7 +34,44 @@ describe("Factory", function () {
         });
         it("Should set the owner", async function () {
             const { factory, deployer } = await loadFixture(deployFactoryFixtures);
-             expect(await factory.owner()).to.equal(deployer.address);
+            expect(await factory.owner()).to.equal(deployer.address);
         });
     });
+
+    describe("Creating", function () {
+        it("Should set the owner", async function () {
+            const { factory, token } = await loadFixture(deployFactoryFixtures);
+            expect(await token.owner()).to.equal(await factory.getAddress());
+        })
+        it("Should set the creator", async function () {
+            const { token, creator } = await loadFixture(deployFactoryFixtures);
+            expect(await token.creator()).to.equal(creator.address);
+        })
+        it("Should set the supply", async function () {
+            const { token, factory } = await loadFixture(deployFactoryFixtures);
+            const totalSupply = ethers.parseUnits("1000000", 18);
+            expect(await token.balanceOf(await factory.getAddress())).to.equal(totalSupply);
+        })
+        it("Should update ETH balance", async function () {
+            const { factory } = await loadFixture(deployFactoryFixtures)
+
+            const balance = await ethers.provider.getBalance(await factory.getAddress())
+
+            expect(balance).to.equal(FEE)
+        })
+        it("Should create the sale", async function () {
+            const { factory, token, creator } = await loadFixture(deployFactoryFixtures)
+      
+            const count = await factory.totalTokens()
+            expect(count).to.equal(1)
+      
+            const sale = await factory.getTokenSale(0)
+      
+            expect(sale.token).to.equal(await token.getAddress())
+            expect(sale.creator).to.equal(creator.address)
+            expect(sale.sold).to.equal(0)
+            expect(sale.raised).to.equal(0)
+            expect(sale.isOpen).to.equal(true)
+          })
+    })
 });
